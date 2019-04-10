c08543c40bcc1e76a463505806669a6a9cd6e5ae
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-10 01:05:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
     
-  elseif phrase == 'I LOVE YOU GRANDMA!'
+  elseif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
   else 
